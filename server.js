2efeae3ae3b05a599054de07e020356567801b61
Astra578 <Aliu.bala777@gmail.com>
2026-05-06 12:32:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
-app.use(express.json({ limit: '800mb' })); app.use(express.urlencoded({ limit: '800mb', extended: true }));;
+app.use(express.json({ limit: '8900mb' })); app.use(express.urlencoded({ limit: '8900mb', extended: true }));;
 
 // NVIDIA NIM API configuration
 const NIM_API_BASE = process.env.NIM_API_BASE || 'https://integrate.api.nvidia.com/v1';
